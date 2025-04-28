0670c541d3b3a4a7bb8d4d8751e8d706fecea392
Nithya Mani <nithya.mn999@gmail.com>
2025-04-28 19:54:18 +0530

    modified for jenkins

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,8 +1,10 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SampleTest extends BaseTest{
     @Test
     public void test() throws Exception {
-        driver.get("https://www.zeiss.com");
+        driver.get("https://www.bstackdemo.com");
+        Assert.assertTrue(driver.getTitle().contains("Sample"));
     }
 }
